8e3abf3df83f5eb7c9e9ab1d465307f287c35d9c
Kirill <kirya.seryogin79@mail.ru>
2025-12-10 21:38:31 +1000

    d

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";   // ← Добавить
+import { HashRouter as Router } from "react-router-dom"; // Измените BrowserRouter на HashRouter
 import "./styles/components.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>      {/* ← ОБЯЗАТЕЛЬНО */}
+    <Router> {}
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
